13f882b133763cd20d87ab6b02f7ca529078d26e
vikas <vikastrivedi@5210.com>
2023-08-25 17:23:11 +0530

    main

---
+++ j.s query/vikas j.query (copy-2)/Scroll revevel/js/script.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal('.headline');
+
+
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 1000 });
+ScrollReveal().reveal('.widget', { interval: 200 });
+
+ScrollReveal().reveal('.flip', {
+    rotate: {
+        x: 20,
+        z: 20
+    }
+});
+
+('.headline').slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null
+};
+
+ScrollReveal().reveal('.Up', { scale: 10 });
+
+ScrollReveal().reveal('.team-card', { interval: 1200, delay: 1000 });
+
+ScrollReveal().reveal('.this', { easing: 'ease-in' });
